e670e144c66bb965c406bd8b6850f1841d0fff99
Muhammad Sobri Maulana <sobri3195@gmail.com>
2026-08-26 03:26:13 +0700

    feat: add adaptive radiotherapy news article

--- src/content/articles/index.ts
+++ src/content/articles/index.ts
@@ -2,6 +2,7 @@ import { diagnosisArticles } from './diagnosis'
 import { treatmentArticles } from './treatment'
 import { supportArticles } from './support'
 import { insightArticles } from './insights'
+import { radiationNewsArticles } from './radiationNews'
 import { categories } from '@/content/categories'
-export const medicalArticles = [...insightArticles, ...diagnosisArticles, ...treatmentArticles, ...supportArticles]
+export const medicalArticles = [...radiationNewsArticles, ...insightArticles, ...diagnosisArticles, ...treatmentArticles, ...supportArticles]
 categories.forEach(category => { category.articleCount = medicalArticles.filter(article => article.categoryId === category.id).length })
